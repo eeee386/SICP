08f94c956adf152f38136a76bb3024b84d424c8f
eeee386 <attilasedon1@gmail.com>
2020-08-18 22:15:14 +0200

    1.3.1

--- chapter1/chapter1.rkt
+++ chapter1/chapter1.rkt
@@ -331,3 +331,28 @@
              (newline) (display "done"))
             (else (timed-prime-test start-range)
                   (search-for-primes (+ 2 start-range) end-range)))))
+
+; 1.3.1
+(define (cube x) (* x x x))
+(define (sum-integers a b)(if (> a b) 0 (+ a (sum-integers (+ a 1) b))))
+(define (sum-cubes a b) (if (> a b) 0 (+ (cube a)(sum-cubes (+ a 1) b))))
+(define (pi-sum a b)(if (> a b) 0 (+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))))
+
+(define (sum term a next b)(if (> a b) 0 (+ (term a)(sum term (next a) next b))))
+
+(define (inc n)(+ n 1))
+(define (sum-cubes-hof a b)(sum cube a inc b))
+
+(define (identity x) x)
+(define (sum-integers-hof a b) (sum identity a inc b))
+
+(define (pi-sum-hof a b)
+  (define (pi-term x) (/ 1.0 (* x (+ x 2))))
+  (define (pi-next x) (+ x 4))
+  (sum pi-term a pi-next b)
+  )
+
+(define (integral f a b dx)
+  (define (add-dx x)(+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b) dx)
+  )
